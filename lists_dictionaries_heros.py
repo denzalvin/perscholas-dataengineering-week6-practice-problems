2892aff2eec7c16b94eb039b7343dc89f34d494f
denzalvin <ddennisalvin@gmail.com>
2022-06-27 08:52:27 -0700

    Dennis-David-solution

--- lists_dictionaries_heros.py
+++ lists_dictionaries_heros.py
@@ -18,10 +18,15 @@ franchise = {
     'marvel' : ['Hulk', 'Thor', 'Black Widow']
 }
 z = [ {'x': 10, 'y': 20} ]
-#1.) How would you change the value 10 in x to 15?  Once you're done x should then be [ [5,2,3], [15,8,9] ].  
+#1.) How would you change the value 10 in x to 15?  Once you're done x should then be [ [5,2,3], [15,8,9] ]. 
+x[1][0] = 15
+print(x)
 #2.) How would you change the hero_name of the first hero from 'Batman' to "Dark Knight"?
+heros[0]['hero_name'] = 'Dark Knight'
 #3.) For the franchise dictionary, how would you change 'Aquaman' to 'Daredevil'?
+franchise['dc'][1] = 'Daredevil'
 #4.) For z, how would you change the value 20 to 30?
+z[0]['y'] = 30
 
 #-----------------------------------------------------------------
 #Problem 2
@@ -36,6 +41,10 @@ superheros = [
 ]
 
 #iterateDictionary(superheros) should output
+def hero_list():
+    for superh in superheros:
+        
+
 
 #real_name - Steve Rogers, hero_name - Captain America
 #real_name - Barry Allan, hero_name - The Flash
